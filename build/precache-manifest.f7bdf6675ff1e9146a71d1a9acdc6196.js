4e568f4a7c2f9c29d685f99d925b7aae3b82a0fa
Oleg Parshyn <olegparshyn@MacBook-Pro-LoopMe-3.local>
2020-01-14 10:35:47 +0200

    reinstall npm packages and removed build folder from gitignore

---
+++ build/precache-manifest.f7bdf6675ff1e9146a71d1a9acdc6196.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01400d0eddda289db28c217956329d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22abc388090dea53568b",
+    "url": "/static/css/main.3e8c57f1.chunk.css"
+  },
+  {
+    "revision": "6eb6475965c4687fbf4c",
+    "url": "/static/js/2.035e398b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.035e398b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22abc388090dea53568b",
+    "url": "/static/js/main.4b34b85e.chunk.js"
+  },
+  {
+    "revision": "13c0b9c086f902cb6895",
+    "url": "/static/js/runtime-main.dabf1df1.js"
+  }
+]);
